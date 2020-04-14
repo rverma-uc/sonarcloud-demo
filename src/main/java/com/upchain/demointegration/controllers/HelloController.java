aba4c789f88a0d59659f05232bca14d1558e15f3
RahulV <rverma@upchain.com>
2020-04-14 18:35:53 -0400

    :tada: Initial Commit

---
+++ src/main/java/com/upchain/demointegration/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.upchain.demointegration.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "This is a demo app to test dev tools integration.";
+    }
+}
