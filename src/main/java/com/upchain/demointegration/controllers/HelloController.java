e2aef4716c6421e437218ad5f27e3bc09da11927
RahulV <rverma@upchain.com>
2020-04-15 21:56:37 -0400

    :art: Random commit

--- src/main/java/com/upchain/demointegration/controllers/HelloController.java
+++ src/main/java/com/upchain/demointegration/controllers/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
+        System.out.println("This is just for changing something.");
+        System.out.println("This is also just for changing something.");
+
         return "This is a demo app to test dev tools integration.";
     }
 }
