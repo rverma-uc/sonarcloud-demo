ee168cc4414e5d1faae2ed90232c1b417159e565
EC2 Default User <ec2-user@ip-10-253-6-203.upc-test-use1-ops.upchain.ops>
2020-04-22 17:22:38 +0000

    Matches code with codeclimate repo

--- src/main/java/com/upchain/demointegration/DemoIntegrationApplication.java
+++ src/main/java/com/upchain/demointegration/DemoIntegrationApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoIntegrationApplication {
 
     public static void main(String[] args) {
+        System.out.println("Started.");
+
         SpringApplication.run(DemoIntegrationApplication.class, args);
     }
 
--- src/main/java/com/upchain/demointegration/controllers/HelloController.java
+++ src/main/java/com/upchain/demointegration/controllers/HelloController.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
+        System.out.println("Test Print");
+        System.out.println("Test Print1");
+        System.out.println("Test Print2");
         return "This is a demo app to test dev tools integration.";
+    }
+
+    public String helper(){
+        return "";
     }
 }
