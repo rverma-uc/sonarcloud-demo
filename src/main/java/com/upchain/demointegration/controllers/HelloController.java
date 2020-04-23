e2c7c53795a100cf83a4e530a3b58437e27fa671
RahulV <rverma@upchain.com>
2020-04-22 20:38:59 -0400

    Adds unused var

--- src/main/java/com/upchain/demointegration/controllers/HelloController.java
+++ src/main/java/com/upchain/demointegration/controllers/HelloController.java
@@ -5,15 +5,83 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private String testUnusedVar;
+
     @RequestMapping("/")
     public String index() {
-        System.out.println("Test Print");
-        System.out.println("Test Print1");
-        System.out.println("Test Print2");
         return "This is a demo app to test dev tools integration.";
     }
 
     public String helper(){
         return "";
+    }
+
+    public String helper2(){
+        double rand = Math.random();
+        if(rand > 0.01){
+            return Double.toString(rand);
+        } else if(rand > 0.01){
+            return Double.toString(rand);
+        } else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        }else if(rand > 0.01){
+            return Double.toString(rand);
+        } else {
+            return Double.toString(rand);
+        }
     }
 }
